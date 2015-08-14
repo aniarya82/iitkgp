1e890d1afdfd0881e2dcf07e6adbe1ee110a8471
sk9727192 <sk9727192@gmail.com>
2015-08-14 06:16:32 +0530

    Create salphp.php

---
+++ salphp.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>MY BLOG</title>
+  <link rel="stylesheet"href="style.css">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<style type="text/css">
+	#emptyrow {height: 20px;}
+</style>
+</head>
+<body>
+
+</script>
+<div class="container">
+<a href="salsignup.php" type="button" class="button button-success">SIGN UP</a>
+
+<div class="row bg-success">
+
+<div class="col-md-4 col-md-offset-8">
+<h5 class="alert alert-danger">ALREADY HAVE AN ACCOUNT!!!</h5>
+<p class="bg-info"><h4><code>SIGNin</code></h4></p>
+<form class="form" method="POST" action="salsignin.php">
+<label for="USERNAME">USERname</label>
+<input type="text" name="form1" placeholder="ENTER USERNAME" class="form-control" id="form1">
+<label for="name">PASSword</label>
+<input type="password" name="form2" placeholder="ENTER PASSWORD" class="form-control" id="form2">
+<br>
+<span class="glyphicon glyphicon-chevron-right"></span>
+<button type="submit" name="bu1" id="butt1" class="btn btn-danger">SIGNin</button>
+
+</form>
+
+</div>
+</div>
+</div>
+</body>
+</html>
